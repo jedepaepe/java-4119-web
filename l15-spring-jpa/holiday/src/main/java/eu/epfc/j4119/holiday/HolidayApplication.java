3291ebfb24bd5375f009591bb6ea2f22783c3bce
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2022-04-26 22:38:04 +0200

    ajout de l'annulation d'un congé

--- l15-spring-jpa/holiday/src/main/java/eu/epfc/j4119/holiday/HolidayApplication.java
+++ l15-spring-jpa/holiday/src/main/java/eu/epfc/j4119/holiday/HolidayApplication.java
@@ -2,20 +2,21 @@ package eu.epfc.j4119.holiday;
 
 import eu.epfc.j4119.holiday.entities.Holiday;
 import eu.epfc.j4119.holiday.repositories.HolidayRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
 
 @Transactional
+@AllArgsConstructor
 @SpringBootApplication
 public class HolidayApplication implements CommandLineRunner {
 	private HolidayRepository db;
-
-	public HolidayApplication() {
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HolidayApplication.class, args);
@@ -23,18 +24,47 @@ public class HolidayApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// 1. créer un enregistrement dans la table Holiday
-		Holiday holiday1 = new Holiday();
-		holiday1.setEmployee("Sylvie");
-		holiday1.setStart("1/5/2022");
-		holiday1.setEnd("20/5/2022");
-		holiday1.setStatus("REQUESTED");
-		db.save(holiday1);
-
-		// 2. lire le contenu de la table holiday
-		List<Holiday> holidays = db.findAll();
-		for(Holiday h : holidays) {
-			System.out.println(h);
-		}
+		Scanner in = new Scanner(System.in);
+		do {
+			System.out.println();
+			System.out.println("Choisissez une des options suivantes:");
+			System.out.println("1. lister les demandes de congé");
+			System.out.println("2. demander un congé");
+			System.out.println("3. annuler un congé");
+			System.out.println("Q. quitter l'application");
+			String choice = in.nextLine();
+			if (choice.equals("Q")) break;
+			switch (choice) {
+				case "1":
+					List<Holiday> holidays = db.findAll();
+					for(Holiday h : holidays) {
+						System.out.println(h);
+					}
+					break;
+				case "2":
+					Holiday holiday = new Holiday();
+					holiday.setEmployee("Sylvie");
+					System.out.print("Début: ");
+					holiday.setStart(in.nextLine());
+					System.out.print("Fin: ");
+					holiday.setEnd(in.nextLine());
+					holiday.setStatus("REQUESTED");
+					db.save(holiday);
+					break;
+				case "3":
+					System.out.print("Id de la demande à annuler: ");
+					Long id = Long.parseLong(in.nextLine());
+					Optional<Holiday> hol = db.findById(id);
+					if (hol.isPresent()) {
+						hol.get().setStatus("CANCELLED");
+						db.save(hol.get());
+					} else {
+						System.out.println("Pas de demande de congé pour l'identifiant " + id);
+					}
+					break;
+				default:
+					System.out.println("Choix non supporté");
+			}
+		} while (true);
 	}
 }
